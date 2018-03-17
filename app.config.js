a87460c0680523d00476f0da47170f330212c66e
onwl007 <onwl007@126.com>
2018-03-17 20:34:30 +0800

    Initial commit

---
+++ app.config.js
@@ -0,0 +1,10 @@
+export default {
+  constant: {
+    menus: [
+      {key: 'index', title: '首页', icon: 'home'},
+      {key: 'archive', title: '分类', icon: 'archive'},
+      {key: 'guestbook', title: '留言墙', icon: 'guestbook'},
+      {key: 'about', title: '关于', icon: 'about'}
+    ]
+  }
+}
---
+++ components/layout/Header/header.vue
@@ -0,0 +1,33 @@
+<template>
+<header class="app-header">
+  <div class="container">
+    <nuxt-link class="nav-logo" to="/">
+      <img src="logo" alt="">
+    </nuxt-link>
+    <div class="nav-menu">
+      <nuxt-link v-for="">
+
+      </nuxt-link>
+    </div>
+  </div>
+</header>
+</template>
+
+<script>
+  import logo from '@/static/images/logo.svg'
+  import config from '@@/app.config'
+
+    export default {
+      name: "header",
+      data(){
+          return {
+            logo,
+            menuItem:config
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
