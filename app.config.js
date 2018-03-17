be80e3b85ede8fb15d13f71d61e06e0444384c17
onwl007 <onwl007@126.com>
2018-03-17 23:24:37 +0800

    配置文件

--- app.config.js
+++ app.config.js
@@ -5,6 +5,6 @@ export default {
       {key: 'archive', title: '分类', icon: 'archive'},
       {key: 'guestbook', title: '留言墙', icon: 'guestbook'},
       {key: 'about', title: '关于', icon: 'about'}
-    ]
+    ],
   }
 }
