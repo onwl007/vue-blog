09a104426bed01d0f8fab87ba0b4c14684ea4452
onwl007 <onwl007@126.com>
2018-03-18 20:54:46 +0800

    添加分享数据

--- app.config.js
+++ app.config.js
@@ -13,5 +13,16 @@ export default {
       {title:'微博',icon:'weibo',url:'https://weibo.com/onwl007'},
       {title:'网易云音乐',icon:'netease-music',url:'http://music.163.com/#/user/home?id=83639314'}
     ],
+    shares:[
+      { key: 'link', title: '复制链接' },
+      { key: 'wechat', title: '微信' },
+      { key: 'weibo', title: '微博' },
+      { key: 'qzone', title: 'QQ空间' },
+      { key: 'douban', title: '豆瓣' },
+      { key: 'evernote', title: '映象笔记' },
+      { key: 'twitter', title: 'Twitter' },
+      { key: 'facebook', title: 'Facebook' },
+      { key: 'mail', title: '邮箱' }
+    ]
   }
 }
