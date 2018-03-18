75f8bca3e96c222570bc70005fbc3c09a4756cb2
onwl007 <onwl007@126.com>
2018-03-18 20:19:21 +0800

    添加社交数据

--- app.config.js
+++ app.config.js
@@ -6,5 +6,12 @@ export default {
       {key: 'guestbook', title: '留言墙', icon: 'guestbook'},
       {key: 'about', title: '关于', icon: 'about'}
     ],
+    socials:[
+      {title:'Email',icon:'email',url:'mailto:onwl007@126.com'},
+      {title:'GitHub',icon:'github',url:'https://github.com/onwl007'},
+      {title:'知乎',icon:'zhihu',url:'https://www.zhihu.com'},
+      {title:'微博',icon:'weibo',url:'https://weibo.com/onwl007'},
+      {title:'网易云音乐',icon:'netease-music',url:'http://music.163.com/#/user/home?id=83639314'}
+    ],
   }
 }
