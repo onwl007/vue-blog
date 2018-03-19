069f4b4dc8c37b0d979f762f507f0d8d0666854c
onwl007 <onwl007@126.com>
2018-03-19 20:59:04 +0800

    初始化

---
+++ components/layout/Side/side.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="aside">
+    <Hots></Hots>
+    <Affix offsetTop="80">
+      <Tags></Tags>
+    </Affix>
+  </div>
+</template>
+
+<script>
+  import Hots from './widgets/Hots'
+  import Tags from './widgets/Tags'
+  import {Affix} from '@/components/common'
+
+  export default {
+    name: "side",
+    components: {
+      Affix,
+      Hots,
+      Tags
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import "side.styl"
+</style>
