562989946c6f173c0c38af3ad337307fd5de7bf9
onwl007 <onwl007@126.com>
2018-03-24 20:50:24 +0800

    初始化

---
+++ components/common/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+export { default as Affix } from './Affix'
+export { default as Card } from './Card'
+export { default as Comment } from './Comment'
+export { default as Editor } from './Editor'
+export { default as Loading } from './Loading'
+export { default as Message } from './Message'
