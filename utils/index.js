a5c402e3a6d01d95b3b4be75990a61e3ba79f899
onwl007 <onwl007@126.com>
2018-04-01 17:16:39 +0800

    初始化

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+/**
+ * @desc Utils entrance
+ */
+
+'use strict'
+
+export * from './tool'
+export * from './storage'
+export * from './validate'
+export * from './dom'
+export * from './comment-meta-parse'
